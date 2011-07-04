8ce8bc2518d80b814b2746e15a9f1b054d2e2944
Gabriel Gilder <ggilder@tractionco.com>
2011-07-03 21:33:41 -0700

    script to compare directories using hashdeep

---
+++ bin/compare_dirs.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ ! -d "$1" ]; then
+	echo "$1 is not a directory!"
+	exit
+fi
+if [ ! -d "$2" ]; then
+	echo "$2 is not a directory!"
+	exit
+fi
+
+HASHLOG=$(mktemp -t compare_dirs)
+
+trap 'rm -f $HASHLOG' EXIT
+
+cd "$1" && hashdeep -rl . > $HASHLOG
+cd "$2" && hashdeep -erla -vv -k $HASHLOG .
+
+exit
